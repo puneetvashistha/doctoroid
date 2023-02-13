ace31361c7799e5f7142bdee4bbdb44c3320290c
Puneet Vashistha <85251288+puneetvashistha@users.noreply.github.com>
2023-02-13 23:15:30 +0530

    Update js.js

--- js.js
+++ js.js
@@ -94,6 +94,130 @@ recognition.onresult = (e) => {
         utter.text = "Sure, I am here to help you out with your costipation problem. There are various ayurvedic home remedies that you can use while at home for costipation like, First one is intake plenty of water and also eating guava would be better. Raisins would be better to consume in morning . Do a brief Yoga Session and intake dairy products. And also do not try to force lower abdomen. Hope it helps.. Do you need Any more help??";
         synth.speak(utter);
     }
+    
+    
+        
+    else if(transcript.includes('asthma')){
+        recognition.stop();
+        utter.text = "Sure, I am here to help you out with your asthma problem. There are various ayurvedic home remedies that you can use while at home for asthma like, it is often treated with herbs such as licorice root, ginger, and turmeric, as well as breathing exercises and lifestyle changes to reduce stress. Hope it helps.. Do you need Any more help??";
+        synth.speak(utter);
+    }
+
+    
+    else if(transcript.includes('diabetes')){
+        recognition.stop();
+        utter.text = "Sure, I am here to help you out with your diabetes problem. There are various ayurvedic home remedies that you can use while at home for diabetes like, First one includes the use of herbs such as fenugreek, gymnema, and bitter melon, along with dietary changes and regular physical activity. Hope it helps.. Do you need Any more help??";
+        synth.speak(utter);
+    }
+
+    
+    else if(transcript.includes('heart')){
+        recognition.stop();
+        utter.text = "Sure, I am here to help you out with your heart related problem. There are various ayurvedic home remedies that you can use while at home for heart problem like, First one is intake plenty of water and use herbs like arjuna, ashwagandha, and Shankhapushpi to improve heart health and reduce stress. Also, avoid junk foods & beverages for healthy lifestylr and perform yoga. Hope it helps.. Do you need Any more help??";
+        synth.speak(utter);
+    }
+
+    
+    else if(transcript.includes('cancer')){
+        recognition.stop();
+        utter.text = "Sure, I am here to help you out with your cancer problem. Generally, in this case one must visit to real healthcare centre for its cure but There are some ayurvedic home remedies that you can use while at home for cancer like  Herbs such as ashwagandha, turmeric, and Guggulu may help strengthen the immune system and improve quality of life. Hope it helps.. Do you need Any more help??";
+        synth.speak(utter);
+    }
+
+    
+    else if(transcript.includes('arthritis')){
+        recognition.stop();
+        utter.text = "Sure, I am here to help you out with your arthritis problem. There are various ayurvedic home remedies that you can use while at home for arthritis like, First one is the use of anti-inflammatory herbs like ginger and turmeric, massage with warm herbal oils, and changes in diet and lifestyle to reduce joint pain and inflammation. Hope it helps.. Do you need Any more help??";
+        synth.speak(utter);
+    }
+
+    
+    else if(transcript.includes('lyme')){
+        recognition.stop();
+        utter.text = "Sure, I am here to help you out with your lyme disease problem. There are various ayurvedic home remedies that you can use while at home for lyme problem like, First one includes the use of herbs like ashwagandha, neem, and guggulu to boost the immune system and improve overall health. Hope it helps.. Do you need Any more help??";
+        synth.speak(utter);
+    }
+
+    
+    else if(transcript.includes('malaria')){
+        recognition.stop();
+        utter.text = "Sure, I am here to help you out with your malaria problem. There are various ayurvedic home remedies that you can use while at home for malaria like, First one includes the use of herbs like neem, tulsi, and chirata to help combat the infection and boost the immune system. Hope it helps.. Do you need Any more help??";
+        synth.speak(utter);
+    }
+
+    
+    else if(transcript.includes('tuberculosis')){
+        recognition.stop();
+        utter.text = "Sure, I am here to help you out with your tuberculosis problem. There are various ayurvedic home remedies that you can use while at home for tuberculosis like, First one involves the use of herbs like ghee, turmeric, and garlic to boost immunity and help the body fight the infection. Also Tuberculosis treatment in government aid hospitals is free, you can cure it there too for free. Hope it helps.. Do you need Any more help??";
+        synth.speak(utter);
+    }
+
+    
+    else if(transcript.includes('aids')){
+        recognition.stop();
+        utter.text = "Sure, I am here to help you out with your aids problem. There are various ayurvedic home remedies that you can use while at home for aids like, Ayurveda can help improve the overall health and well-being of people with HIV/AIDS through the use of immune-boosting herbs, dietary changes, and stress-reducing techniques. Hope it helps.. Do you need Any more help??";
+        synth.speak(utter);
+    }
+
+    
+    else if(transcript.includes('stroke')){
+        recognition.stop();
+        utter.text = "Sure, I am here to help you out with your stroke problem. There are various ayurvedic home remedies that you can use while at home for stroke like, First one includes the use of herbs like Shankhapushpi and Brahmi, along with massage and physical therapy, to improve brain function and support recovery. Hope it helps.. Do you need Any more help??";
+        synth.speak(utter);
+    }
+
+    
+    else if(transcript.includes('parkinson')){
+        recognition.stop();
+        utter.text = "Sure, I am here to help you out with your parkinson problem. There are various ayurvedic home remedies that you can use while at home for parkinson like, First one includes the use of herbs like Shankhapushpi, Ashwagandha, and Vidari to improve brain function and reduce symptoms. Hope it helps.. Do you need Any more help??";
+        synth.speak(utter);
+    }
+
+    
+    else if(transcript.includes('infection')){
+        recognition.stop();
+        utter.text = "Sure, I am here to help you out with your infection problem. There are various ayurvedic home remedies that you can use while at home for infection like, It includes the use of antimicrobial herbs like uva ursi and cranberry. Hope it helps.. Do you need Any more help??";
+        synth.speak(utter);
+    }
+
+    
+    else if(transcript.includes('sinusitis')){
+        recognition.stop();
+        utter.text = "Sure, I am here to help you out with your sinusitis problem. There are various ayurvedic home remedies that you can use while at home for sinusitis like, It includes the use of decongestant herbs like eucalyptus and peppermint, along with steam inhalation and dietary changes to improve sinus health. Hope it helps.. Do you need Any more help??";
+        synth.speak(utter);
+    }
+
+    
+    else if(transcript.includes('gas')){
+        recognition.stop();
+        utter.text = "Sure, I am here to help you out with your gastritis problem. Ayurvedic remedies for gastritis may include the use of digestive-supporting herbs like fennel and cumin, along with dietary changes to reduce irritation of the stomach lining. Hope it helps.. Do you need Any more help??";
+        synth.speak(utter);
+    }
+
+    
+    else if(transcript.includes('kidney')){
+        recognition.stop();
+        utter.text = "Sure, I am here to help you out with your kidney problem. Ayurvedic remedies for chronic kidney disease may include the use of diuretic herbs like dandelion and coriander, along with dietary changes to reduce stress on the kidneys. Also, try to intake plenty of water.. Hope it helps.. Do you need Any more help??";
+        synth.speak(utter);
+    }
+
+        
+    else if(transcript.includes('anemia')){
+        recognition.stop();
+        utter.text = "Sure, I am here to help you out with your anemia problem. Ayurvedic remedies for anemia may include the use of iron-rich herbs like dandelion and alfalfa, along with dietary changes to increase iron intake. Also beetroot and pomegranate helps to cure it.. Hope it helps.. Do you need Any more help??";
+        synth.speak(utter);
+    }
+
+    else if(transcript.includes('depression')){
+        recognition.stop();
+        utter.text = "Sure, I am here to help you out with your Tension problem. Ayurvedic remedies for depression may include the use of herbs like ashwagandha and Shankhapushpi, along with yoga, meditation, and other stress-reducing techniques. Hope it helps.. Do you need Any more help??";
+        synth.speak(utter);
+    }
+
+
+    
+    
+    
 
     else if(transcript.includes('thank')){
         recognition.stop();
