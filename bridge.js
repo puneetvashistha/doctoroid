b2fcb7cb7276409d56c1824c5a7233e82af280db
Puneet Vashistha <85251288+puneetvashistha@users.noreply.github.com>
2023-02-14 22:01:06 +0530

    Update bridge.js

--- bridge.js
+++ bridge.js
@@ -8,7 +8,7 @@ recognition.maxAlternatives = 2;
 const synth = window.speechSynthesis;
 
 startBtn.addEventListener("click", () => {
-let utter = new SpeechSynthesisUtterance("");
+let utter = new SpeechSynthesisUtterance("Hello, I am your DOCTOROID an AI Ayurvedic Doctor. Here we are going to perform Bridge pose. Bridge pose promotes core and glute strength and, in doing so, helps prevent back pain. This pose simultaneously strengthens your core and your lower back muscles, Kindly be ready and take a deep breath.. First Lie on your back with your arms by your sides as shown on display. Now With your feet about hip-width apart, bend your knees, keeping your feet flat on the floor directly under your knees as shown on display. Now Engage your core and glute muscles, raise your hips so that your body forms a straight line from your knees to your shoulders as shown on display. Now wait for one minute and perform it almost 5-6 times.. Let's count.. 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60..  Very Nice, we have done it.. Do you want to perform it again??");
 synth.speak(utter);
 utter.onend= () => {
     recognition.start();
