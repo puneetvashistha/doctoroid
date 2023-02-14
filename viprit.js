6149932feb9f8de6a70c785d647d598f578b1071
Puneet Vashistha <85251288+puneetvashistha@users.noreply.github.com>
2023-02-14 21:56:26 +0530

    Update viprit.js

--- viprit.js
+++ viprit.js
@@ -8,13 +8,6 @@ recognition.maxAlternatives = 2;
 const synth = window.speechSynthesis;
 
 startBtn.addEventListener("click", () => {
-    var greeting = new SpeechSynthesisUtterance("Hello, I am your DOCTOROID an AI Ayurvedic Doctor. Here we are going to perform Legs up the Wall pose. Kindly be ready and take a deep breath................... Hope you are ready?? First Sit with your right side flush against a wall as shown at display. Now Rest your left elbow on the floor, rotate and raise your legs up the wall while lowering your torso so that you’re lying on your back with your heels against the wall as shown in display. Now wait for one minute and perform it almost 5-6 times.. Let's count.. 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60.. Very Nice, we have done it.. Do you want to perform it again??");
-    window.speechSynthesis.speak(greeting);
-    setTimeout(function() {
-        recognition.start();
-      }, 100000);
-});
-
 let utter = new SpeechSynthesisUtterance("");
 synth.speak(utter);
 utter.onend= () => {
@@ -56,7 +49,7 @@ recognition.onresult = (e) => {
     }
 
 
-}
+}});
 
 document.addEventListener("contextmenu", function (e) {
     e.preventDefault();
